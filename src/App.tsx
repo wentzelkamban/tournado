735fbbb564f7f5f0cad0f11124ed5da5594cb289
Haukur Hlöðversson <haukur@haukurh.is>
2021-11-14 16:52:38 +0000

    Added router to app + Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          -- Tournado --
-        </a>
-      </header>
-    </div>
+    <>
+      <header>Tournado</header>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
+        corrupti quos dolores et qAt vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+        voluptatum del
+      </p>
+    </>
+  );
+};
+
+export default Home;
